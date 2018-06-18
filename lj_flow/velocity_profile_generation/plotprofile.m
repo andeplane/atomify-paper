0b8adbf5e746316d2f0f80c05bcd997d9a4c3437
Anders Hafreager <andershaf@gmail.com>
2018-06-18 13:42:49 +0200

    Fixing text and plots

--- lj_flow/velocity_profile_generation/plotprofile.m
+++ lj_flow/velocity_profile_generation/plotprofile.m
@@ -8,15 +8,16 @@ hfig1 = figure;
 hax1 = axes;
 set(hfig1, 'units', 'centimeters')
 set(hax1, 'fontsize', 7)
+stride = 3
 
-plot(rho0_002(:,4), rho0_002(:,7), 'linewidth', 1.5, 'marker', 'o');
+plot(rho0_002(1:stride:end,4), rho0_002(1:stride:end,7)/mean(rho0_002(1:3:end,7)), 'linewidth', 1.5, 'marker', 'o');
 hold on
-plot(rho0_005(:,4), rho0_005(:,7), 'linewidth', 1.5, 'marker', '<');
-plot(rho0_01(:,4), rho0_01(:,7), 'linewidth', 1.5, 'marker', '>');
-plot(rho0_1(:,4), rho0_1(:,7), 'linewidth', 1.5, 'marker', 's');
-plot(rho0_5(:,4), rho0_5(:,7), 'linewidth', 1.5, 'marker', 'd');
+plot(rho0_005(1:stride:end,4), rho0_005(1:stride:end,7)/mean(rho0_005(1:stride:end,7)), 'linewidth', 1.5, 'marker', '<');
+plot(rho0_01(1:stride:end,4), rho0_01(1:stride:end,7)/mean(rho0_01(1:stride:end,7)), 'linewidth', 1.5, 'marker', '>');
+plot(rho0_1(1:stride:end,4), rho0_1(1:stride:end,7)/mean(rho0_1(1:stride:end,7)), 'linewidth', 1.5, 'marker', 's');
+plot(rho0_5(1:stride:end,4), rho0_5(1:stride:end,7)/mean(rho0_5(1:stride:end,7)), 'linewidth', 1.5, 'marker', 'd');
 
 set(hfig1, 'position', [0 0 8.6 8.6]);
 legend({'\rho_m = 0.002', '\rho_m = 0.005', '\rho_m = 0.01', '\rho_m = 0.1', '\rho_m = 0.5'});
 xlabel('r')
-ylabel('v(r)')
+ylabel('v^*(r)')
